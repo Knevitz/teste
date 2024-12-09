668dccec5c957c8f3f60c9b79243905f687f25c3
Knevitz <pedroekpedroso@gmail.com>
2024-12-08 22:32:06 -0300

    pagina login

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Destaca links de navegação e itens do dropdown
 const currentLocation = window.location.pathname.split('/').pop();
 const navLinks = document.querySelectorAll('.nav-link');
 const dropdownItems = document.querySelectorAll('.dropdown-item');
@@ -15,4 +16,9 @@ dropdownItems.forEach(item => {
     item.addEventListener('click', () => {
         document.querySelector('#navbarDropdown').classList.add('active');
     });
+});
+
+// Aplica máscara ao campo de CNPJ
+$(document).ready(function () {
+    $('#cnpj').inputmask("99.999.999/9999-99", { placeholder: " " });
 });
